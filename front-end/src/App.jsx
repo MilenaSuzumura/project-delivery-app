1feafe960f64b00e0d1a4bd8a1537a76963e1e90
Bruno <brunocfaraco@hotmail.com.br>
2023-02-28 20:50:31 -0300

    feat: implemented route /register

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Provider from './context/Provider';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={ () => <Redirect to="/login" /> } />
           <Route exact path="/login" component={ Login } />
+          <Route exact path="/register" component={ Register } />
         </Switch>
       </Provider>
     </div>
