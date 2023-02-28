d178d5095286eefb8d873dd191a0925c8ead70c8
Bruno <brunocfaraco@hotmail.com.br>
2023-02-28 18:25:33 -0300

    feat: implemented email and password validation with component contexts

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,49 +1,83 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// import api from '../utils/api';
 
 const loginTestId = 'common_login__';
 
 export default function Login() {
-  const [emailValue, changeEmail] = useState('');
-  const [passwordValue, changePassword] = useState('');
-  const [loginResponse, changeLoginResponse] = useState(false);
+  const history = useHistory();
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [loginResponse, setLoginResponse] = useState(true);
+  const [loginAvailable, setLoginAvailable] = useState(false);
 
-  const login = () => {
-    changeLoginResponse(false);
+  useEffect(() => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^\w{6}/;
+
+    if (passwordRegex.test(passwordValue) && emailRegex.test(emailValue)) {
+      setLoginAvailable(true);
+    } else {
+      setLoginAvailable(false);
+    }
+  }, [emailValue, passwordValue]);
+
+  const login = async () => {
+    // const body = {
+    //   email: emailValue,
+    //   password: passwordValue,
+    // };
+
+    try {
+      // const response = await api.post('/user', body);
+      const response = { token: 'hrn7u8i923ihf23978wg3289' };
+      console.log(response);
+    } catch (e) {
+      setLoginResponse(false);
+    }
+  };
+
+  const redirect = () => {
+    history.push('/register');
   };
 
   return (
     <div className="Login">
       <form>
+        Login
         <input
           type="email"
           value={ emailValue }
-          onChange={ (e) => changeEmail(e.target.value) }
+          onChange={ (e) => setEmailValue(e.target.value) }
           data-testid={ `${loginTestId}input-email` }
         />
+        Senha
         <input
           type="password"
           value={ passwordValue }
-          onChange={ (e) => changePassword(e.target.value) }
+          min="6"
+          onChange={ (e) => setPasswordValue(e.target.value) }
           data-testid={ `${loginTestId}input-password` }
         />
         <button
           type="submit"
           onClick={ () => login() }
           data-testid={ `${loginTestId}button-login` }
+          disabled={ !loginAvailable }
         >
           LOGIN
         </button>
         <button
           type="button"
-          onClick={ <Redirect to="/register" /> }
+          onClick={ () => redirect() }
           data-testid={ `${loginTestId}button-register` }
         >
           Ainda não tenho conta
         </button>
       </form>
       {
-        loginResponse && (
+        !loginResponse && (
           <text
             data-testid={ `${loginTestId}element-invalid-email` }
           >
