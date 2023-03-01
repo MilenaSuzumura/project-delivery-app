7456c134fa86ee6ffa2f09dcb8c87174d024b4f6
Bruno <brunocfaraco@hotmail.com.br>
2023-03-01 18:22:57 -0300

    feat: implemented func to get data in browser

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,7 +4,17 @@ const userRouter = require('../routers/user.router');
 
 const app = express();
 
+// Função que serbe para que se possa testar as requisições pelo navegador web, não influencia nos testes 
+// mas mantive comentada pois não é necessária para que os mesmos rodem.
+// Feito por: Bruno Faraco com ajuda do ChatGPT fazendo a seguinte pergunta:
+// "como resolver o problema "Access-Control-Allow-Origin" do Axios em React"
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json());
+
 
 app.use(userRouter);
 
