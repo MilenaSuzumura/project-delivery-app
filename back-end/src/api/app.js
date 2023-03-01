49e01984111a9d6f8d52b33a50ec9bb8132868c9
Bruno <brunocfaraco@hotmail.com.br>
2023-03-01 19:20:30 -0300

    refactor: anonymous func refactored to solve lint problems

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,13 +8,12 @@ const app = express();
 // mas mantive comentada pois não é necessária para que os mesmos rodem.
 // Feito por: Bruno Faraco com ajuda do ChatGPT fazendo a seguinte pergunta:
 // "como resolver o problema "Access-Control-Allow-Origin" do Axios em React"
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(express.json());
-
 
 app.use(userRouter);
 
