839c5a8aa87062e03c2e17e25e25e3cf4fc2ef35
Bruno <brunocfaraco@hotmail.com.br>
2023-03-02 12:44:07 -0300

    feat: added route por /customer/products

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,6 +5,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Provider from './context/Provider';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Products from './pages/Products';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" render={ () => <Redirect to="/login" /> } />
           <Route exact path="/login" component={ Login } />
           <Route exact path="/register" component={ Register } />
+          <Route exact path="/customer/products" component={ Products } />
         </Switch>
       </Provider>
     </div>
