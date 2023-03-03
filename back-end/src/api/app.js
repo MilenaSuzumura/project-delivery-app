042926c4e4007ab6e4012245bc5bea0809ae5b1f
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-03 15:54:38 -0300

    style: update formatting in app

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -5,16 +5,17 @@ const customerProductsRouter = require('../routers/customer-products.router');
 
 const app = express();
 
-// Função que serbe para que se possa testar as requisições pelo navegador web, não influencia nos testes 
+app.use(express.json());
+
+// Função que serve para que se possa testar as requisições pelo navegador web, não influencia nos testes 
 // mas mantive comentada pois não é necessária para que os mesmos rodem.
 // Feito por: Bruno Faraco com ajuda do ChatGPT fazendo a seguinte pergunta:
 // "como resolver o problema "Access-Control-Allow-Origin" do Axios em React"
-app.use((req, res, next) => {
+app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(express.json());
 
 app.use(userRouter);
 app.use(customerProductsRouter);
