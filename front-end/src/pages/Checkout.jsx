61d9635dfd7ab4d71bb8e1f175b867130fee1bc5
Bruno <brunocfaraco@hotmail.com.br>
2023-03-05 01:55:14 -0300

    feat: starting logic to create card just to item that was ordered

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom';
+
+import NavBar from '../components/NavBar';
+import OrderCard from '../components/OrderCard';
+import getFromLocalStorage from '../utils/localStorage';
+
+// const CHECKOUT = 'customer_checkout__';
+// const ELEMENT = 'element-order-';
 
 export default function Checkout() {
+  const name = getFromLocalStorage('user', 'name');
+  const role = getFromLocalStorage('user', 'role');
+  // const history = useHistory();
+
+  const [carItems, setCarItems] = useState([]);
+  const [itemsToRender, setItemsToRender] = useState([]);
+
+  const userInfos = { name, role };
+  // console.log(carItems);
+  useEffect(() => {
+    setCarItems(JSON.parse(getFromLocalStorage('carItems')));
+    const test = carItems.some((e) => e.itemAmount > 0);
+    console.log(test);
+    if (test) {
+      const filteredItems = carItems.filter((carItem) => carItem.itemAmount > 0);
+      setItemsToRender(filteredItems);
+    }
+  }, [carItems]);
+
   return (
     <div className="checkout">
-      a
+      <NavBar userInfos={ userInfos } />
+      { itemsToRender.map((carItem) => (
+        <OrderCard key={ carItem.id } carItems={ carItem } />
+      )) }
     </div>
   );
 }
