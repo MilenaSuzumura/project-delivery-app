1d85f56fc3c710bda57fc7baf1d134a54932a1ce
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-06 16:45:14 -0300

    feat: add new routes and validateToken in customer router

--- back-end/src/routers/customer.router.js
+++ back-end/src/routers/customer.router.js
@@ -1,21 +1,25 @@
 const express = require('express');
 
 const customerController = require('../controllers/customer.controller');
+const validateToken = require('../middlewares/validateToken');
 
 const router = express.Router();
 
 const productById = '/products/:id';
+const salesById = '/sales/:id';
 
 router.get('/products', customerController.getAllProducts);
 router.get(productById, customerController.getProductById);
 router.get('/sales', customerController.getAllSales);
-router.get('/sales/:id', customerController.getSaleById);
+router.get(salesById, customerController.getSaleById);
 
+router.post('/checkout', validateToken, customerController.checkout);
 router.post('/products', customerController.createProduct);
-router.post('/sales', customerController.createSale);
 
 router.put(productById, customerController.updateProduct);
+router.put(salesById, customerController.updateSale);
 
 router.delete(productById, customerController.deleteProduct);
+router.delete(salesById, customerController.deleteSale);
 
 module.exports = router;
