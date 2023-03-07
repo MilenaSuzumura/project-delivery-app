f4d07790b79aa258e9c85f254a234288fd33146a
Bruno <brunocfaraco@hotmail.com.br>
2023-03-07 05:02:16 -0300

    feat: implemented fetchSimulator file

--- front-end/src/pages/CustomerOrders.jsx
+++ front-end/src/pages/CustomerOrders.jsx
@@ -1,34 +1,8 @@
 import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import OrderStatusCard from '../components/OrderStatusCard';
+import fetchSimulator from '../utils/fetchSimulator';
 import getFromLocalStorage from '../utils/localStorage';
-
-const TIMEOUT = 1000;
-const fetchSimulator = () => new Promise((resolve) => {
-  setTimeout(() => {
-    const data = [
-      {
-        id: 1,
-        status: 'Pendente',
-        saleDate: '08042021',
-        totalPrice: 23.80,
-      },
-      {
-        id: 20,
-        status: 'Preparando',
-        saleDate: '08042021',
-        totalPrice: 14.20,
-      },
-      {
-        id: 349,
-        status: 'Entregue',
-        saleDate: '07042021',
-        totalPrice: 28.46,
-      },
-    ];
-    resolve(data);
-  }, TIMEOUT);
-});
 
 function CustomerOrders() {
   const name = getFromLocalStorage('user', 'name');
