466e4596d31467b7f650dfbf30b38f79e878f19b
GustavoDimeira <gustavodimeira1@gmail.com>
2023-03-07 17:08:08 -0400

    feat: checkout screen

--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import NavBar from '../components/NavBar';
 import OrderCard from '../components/OrderCard';
 
@@ -11,7 +13,10 @@ export default function Checkout() {
   const role = getFromLocalStorage('user', 'role');
 
   const [cartItems, setCartItems] = useState([]);
+
   const [totalPrice, setTotalPrice] = useState(0);
+  const [deliveryAddress, setDeliveryAddress] = useState('');
+  const [deliveryNumber, setDeliveryNumber] = useState('');
 
   const userInfos = { name, role };
 
@@ -31,6 +36,42 @@ export default function Checkout() {
 
     handleStorageChange();
   }, []);
+
+  const changeInputValue = (e) => {
+    switch (e.target.id) {
+    case 'address':
+      setDeliveryAddress(e.target.value);
+      break;
+    case 'number':
+      setDeliveryNumber(e.target.value);
+      break;
+    default:
+      console.log('500');
+    }
+  };
+
+  const makeRequest = async () => {
+    const body = {
+      totalPrice,
+      deliveryAddress,
+      deliveryNumber,
+    };
+
+    const headers = { 'Content-Type': 'application/json' };
+
+    try {
+      const response = await axios({
+        method: 'post',
+        url: 'http://localhost:3001/customer/sales',
+        data: body,
+        headers,
+      });
+
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   return (
     <div className="checkout">
@@ -71,36 +112,41 @@ export default function Checkout() {
         Detalhes e Endereço para Entrega
       </h3>
       <form>
-        <div>
-          <label
-            data-testid={ `${CUSTOMER_TEST_ID}select-seller` }
-            htmlFor="saller"
-          >
-            P. Vendedora Responsável:
-            <input type="text" id="seller" required />
-          </label>
-          <label
+        <label htmlFor="saller">
+          P. Vendedora Responsável:
+          <select id="saller" data-testid={ `${CUSTOMER_TEST_ID}select-seller` }>
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+        </label>
+        <label htmlFor="address">
+          Endereço:
+          <input
+            type="text"
+            id="address"
+            value={ deliveryAddress }
+            onChange={ (e) => changeInputValue(e) }
             data-testid={ `${CUSTOMER_TEST_ID}input-address` }
-            htmlFor="address"
-          >
-            Endereço:
-            <input type="text" id="address" required />
-          </label>
-          <label
+          />
+        </label>
+        <label htmlFor="number">
+          Número:
+          <input
+            type="text"
+            id="number"
+            value={ deliveryNumber }
+            onChange={ (e) => changeInputValue(e) }
             data-testid={ `${CUSTOMER_TEST_ID}input-address-number` }
-            htmlFor="number"
-          >
-            Número:
-            <input type="text" id="number" required />
-          </label>
-          <button
-            data-testid={ `${CUSTOMER_TEST_ID}button-submit-order` }
-            type="button"
-            onClick={ () => console.log('a') }
-          >
-            FINALIZAR PEDIDO
-          </button>
-        </div>
+          />
+        </label>
+        <button
+          data-testid={ `${CUSTOMER_TEST_ID}button-submit-order` }
+          type="button"
+          onClick={ () => makeRequest() }
+        >
+          FINALIZAR PEDIDO
+        </button>
       </form>
     </div>
   );
