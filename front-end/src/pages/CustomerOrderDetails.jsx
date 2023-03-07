c361109303262b28570cbd9a620d416f4c9a6101
Bruno <brunocfaraco@hotmail.com.br>
2023-03-07 17:01:56 -0300

    WIP: adapting

--- front-end/src/pages/CustomerOrderDetails.jsx
+++ front-end/src/pages/CustomerOrderDetails.jsx
@@ -1,57 +1,105 @@
-import axios from 'axios';
+// import axios from 'axios';
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import getFromLocalStorage from '../utils/localStorage';
 
 const DETAILS = 'customer_order_details__';
 const ELEMENT = 'element-order-';
 
+const fetchSimulator = () => new Promise((resolve) => {
+  const TIMEOUT = 1000;
+  const data = {
+    id: 1,
+    userId: 3,
+    sellerId: 2,
+    totalPrice: '15.00',
+    deliveryAddress: 'rua A',
+    deliveryNumber: '2',
+    saleDate: '2023-03-06T18:57:49.000Z',
+    status: 'Pendente',
+    products: [
+      {
+        id: 2,
+        name: 'Heineken 600ml',
+        price: '7.50',
+        urlImage: 'http://localhost:3001/images/heineken_600ml.jpg',
+        SaleProduct: {
+          saleId: 1,
+          productId: 2,
+          quantity: 2,
+        },
+      },
+    ],
+    seller: {
+      id: 2,
+      name: 'Fulana Pereira',
+      email: 'fulana@deliveryapp.com',
+      password: '3c28d2b0881bf46457a853e0b07531c6',
+      role: 'seller',
+    },
+  };
+
+  setTimeout(() => {
+    resolve(data);
+  }, TIMEOUT);
+});
 function CustomerOrderDetails({ match }) {
   const { id } = match.params;
 
   const name = getFromLocalStorage('user', 'name');
   const role = getFromLocalStorage('user', 'role');
-  const token = getFromLocalStorage('user', 'token');
+  // const token = getFromLocalStorage('user', 'token');
 
-  const [orders, setOrders] = useState([]);
+  const [details, setDetails] = useState([]);
 
   useEffect(() => {
     const getFetch = async () => {
-      const headers = { 'Content-Type': 'application/json', authorization: token };
-
       try {
-        const data = await axios({
-          method: 'get',
-          url: `http://localhost:3001/customer/orders/${id}`,
-          data: {},
-          headers,
-        });
-        setOrders(data);
+        const response = await fetchSimulator();
+        setDetails(response);
       } catch (error) {
         console.log(error);
       }
     };
 
     getFetch();
-  }, [id, token]);
+  }, []);
+  // useEffect(() => {
+  //   const getFetch = async () => {
+  //     const headers = { 'Content-Type': 'application/json', authorization: token };
 
-  console.log(orders);
+  //     try {
+  //       const data = await axios({
+  //         method: 'get',
+  //         url: `http://localhost:3001/customer/orders/${id}`,
+  //         data: {},
+  //         headers,
+  //       });
+  //       setOrders(data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   getFetch();
+  // }, [id, token]);
+
+  console.log(details);
 
   const handleBtn = () => {
     console.log('SENDO CLICADO');
   };
 
-  return (
-    <div>
-      <NavBar userInfos={ { name, role } } />
-      <div>
-        Detalhe do Pedido
+  const detailsGenerator = () => {
+    if (details.length > 0) {
+      return (
         <div>
           <p data-testid={ `${DETAILS}${ELEMENT}details-label-order-id` }>
             { `Pedido ${id}` }
           </p>
           <p data-testid={ `${DETAILS}${ELEMENT}details-label-seller-name` }>
-            {/* { `P. Vend: ${sellerName}` } */}
+            { `P. Vend: ${details.seller.name}` }
           </p>
           <p data-testid={ `${DETAILS}${ELEMENT}details-label-order-date` }>
             {/* { date } */}
@@ -85,6 +133,16 @@ function CustomerOrderDetails({ match }) {
             MARCAR COMO ENTREGUE
           </button>
         </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      {/* <NavBar userInfos={ { name, role } } /> */}
+      <div>
+        DETALHE DO PEDIDO
+        { detailsGenerator() }
       </div>
     </div>
   );
