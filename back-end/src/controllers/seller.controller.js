48692c74621b8d7da70f471fc5cd69e0248ffbea
Luiz Pereira <lfhbpereira@gmail.com>
2023-03-07 19:20:22 -0300

    feat: add functions in seller controller

---
+++ back-end/src/controllers/seller.controller.js
@@ -0,0 +1,17 @@
+const sellerService = require('../services/seller.service');
+
+const getOrdersByUser = async (req, res) => {
+  const { sellerId } = req.body;
+  const { message } = await sellerService.getOrdersByUser(sellerId);
+
+  return res.status(200).json(message);
+};
+
+const getOrderById = async (req, res) => {
+  const { id } = req.params;
+  const { message } = await sellerService.getOrderById(id);
+
+  return res.status(200).json(message);
+};
+
+module.exports = { getOrdersByUser, getOrderById };
