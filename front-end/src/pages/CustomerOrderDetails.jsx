3bfee02310069ff56f907f7426c3a5e04475ea54
Bruno <brunocfaraco@hotmail.com.br>
2023-03-07 15:38:28 -0300

    feat: customer order details data-testids implementation

--- front-end/src/pages/CustomerOrderDetails.jsx
+++ front-end/src/pages/CustomerOrderDetails.jsx
@@ -1,10 +1,65 @@
 import PropTypes from 'prop-types';
+import NavBar from '../components/NavBar';
+import getFromLocalStorage from '../utils/localStorage';
+
+const DETAILS = 'customer_order_details__';
+const ELEMENT = 'element-order-';
 
 function CustomerOrderDetails({ match }) {
   const { id } = match.params;
+
+  const name = getFromLocalStorage('user', 'name');
+  const role = getFromLocalStorage('user', 'role');
+
+  const handleBtn = () => {
+    console.log('SENDO CLICADO');
+  };
+
   return (
     <div>
-      {id}
+      <NavBar userInfos={ { name, role } } />
+      <div>
+        Detalhe do Pedido
+        <div>
+          <p data-testid={ `${DETAILS}${ELEMENT}details-label-order-id` }>
+            { `Pedido ${id}` }
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}details-label-seller-name` }>
+            {/* { `P. Vend: ${sellerName}` } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}details-label-order-date` }>
+            {/* { date } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}details-label-delivery-status<index>` }>
+            {/* { status } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}table-item-number-<index>` }>
+            {/* { itemNumber } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}table-name-<index>` }>
+            {/* { itemName } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}table-quantity-<index>` }>
+            {/* { quantity } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}table-unit-price-<index>` }>
+            {/* { unitPrice } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}table-sub-total-<index>` }>
+            {/* { subTotal } */}
+          </p>
+          <p data-testid={ `${DETAILS}${ELEMENT}total-price` }>
+            {/* { totalPrice } */}
+          </p>
+          <button
+            data-testid="customer_order_details__button-delivery-check"
+            type="button"
+            onClick={ () => handleBtn }
+          >
+            MARCAR COMO ENTREGUE
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
@@ -18,3 +73,4 @@ CustomerOrderDetails.propTypes = {
 };
 
 export default CustomerOrderDetails;
+// { `${DETAILS}${ELEMENT}` }
